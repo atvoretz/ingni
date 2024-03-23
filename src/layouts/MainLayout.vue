9b2328f3723efe2fe4e759d6fd115c0cc1e74a8b
atvoretz <30846353+atvoretz@users.noreply.github.com>
2024-03-23 15:00:53 +0400

    3 files modified

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,30 +2,94 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
+        <!-- Динамически изменяем иконку и действие кнопки -->
         <q-btn
           flat
           dense
           round
-          icon="menu"
+          :icon="showSearch ? 'arrow_back' : 'menu'"
           aria-label="Menu"
-          @click="toggleLeftDrawer"
+          @click="showSearch ? toggleSearch() : toggleLeftDrawer()"
         />
 
         <q-toolbar-title>ingeni</q-toolbar-title>
 
-        <div>alpha v0.0.1</div>
+        <div class="flex row items-center q-gutter-sm">
+          <!-- Строка поиска, скрыта по умолчанию -->
+          <q-input
+            filled
+            v-model="searchText"
+            v-show="showSearch"
+            dense
+            debounce="300"
+            placeholder="Поиск..."
+            class="col"
+          />
+          <q-btn
+            flat
+            dense
+            round
+            icon="search"
+            aria-label="Search"
+            @click="toggleSearch"
+          />
+        </div>
       </q-toolbar>
+      <q-tabs
+        v-model="tab"
+        @update:model-value="handleTabClick"
+        dense
+        no-caps
+        class="bg-primary text-white"
+        align="justify"
+      >
+        <q-tab
+          v-for="link in essentialLinks"
+          :key="link.title"
+          :name="link.title"
+          :label="link.title"
+        />
+      </q-tabs>
     </q-header>
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-        <q-item-label header>Ссылки</q-item-label>
+        <q-item dense>
+          <q-item-section avatar>
+            <q-avatar size="50px">
+              <img src="https://ingeni.app/brand/avatar.png" />
+            </q-avatar>
+          </q-item-section>
 
-        <EssentialLink
+          <q-item-section>
+            <q-item-label lines="1"
+              ><strong>Андрей Тарасов</strong></q-item-label
+            >
+            <q-item-label caption lines="1">ИП ТАРАСОВ</q-item-label>
+          </q-item-section>
+
+          <q-item-section side top>
+            <q-btn icon="brightness_4" flat @click="toggleDarkMode" />
+          </q-item-section>
+        </q-item>
+
+        <q-separator spaced />
+
+        <q-item
+          dense
           v-for="link in essentialLinks"
           :key="link.title"
-          v-bind="link"
-        />
+          clickable
+        >
+          <q-item-section avatar>
+            <q-icon :name="link.icon" />
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label lines="1">{{ link.title }}</q-item-label>
+            <q-item-label caption lines="1">{{ link.caption }}</q-item-label>
+          </q-item-section>
+        </q-item>
       </q-list>
     </q-drawer>
 
@@ -36,8 +100,9 @@
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue';
-import EssentialLink from 'components/EssentialLink.vue';
+import { defineComponent, ref, onMounted } from 'vue';
+import { useQuasar } from 'quasar';
+import { useRouter } from 'vue-router';
 
 const linksList = [
   {
@@ -69,19 +134,52 @@ const linksList = [
 export default defineComponent({
   name: 'MainLayout',
 
-  components: {
-    EssentialLink,
-  },
-
   setup() {
     const leftDrawerOpen = ref(false);
+    const showSearch = ref(false);
+    const searchText = ref('');
+    const $q = useQuasar();
+    const tab = ref('');
+    const essentialLinks = ref(linksList); // Объявляем как реактивное свойство
+    const router = useRouter(); // Получаем доступ к экземпляру роутера
+
+    const toggleDarkMode = () => {
+      $q.dark.set(!$q.dark.isActive);
+    };
+
+    const toggleLeftDrawer = () => {
+      leftDrawerOpen.value = !leftDrawerOpen.value;
+    };
+
+    const toggleSearch = () => {
+      showSearch.value = !showSearch.value;
+    };
+
+    const handleTabClick = (tabTitle) => {
+      const link = essentialLinks.value.find(
+        (link) => link.title === tabTitle
+      )?.link;
+      if (link) {
+        router.push(link);
+      }
+    };
+
+    onMounted(() => {
+      if (essentialLinks.value.length > 0) {
+        tab.value = essentialLinks.value[0].title; // Теперь корректно используем .value
+      }
+    });
 
     return {
-      essentialLinks: linksList,
       leftDrawerOpen,
-      toggleLeftDrawer() {
-        leftDrawerOpen.value = !leftDrawerOpen.value;
-      },
+      showSearch,
+      searchText,
+      toggleLeftDrawer,
+      toggleSearch,
+      toggleDarkMode,
+      tab,
+      essentialLinks,
+      handleTabClick,
     };
   },
 });
--- src/pages/ErrorNotFound.vue
+++ src/pages/ErrorNotFound.vue
@@ -5,7 +5,9 @@
     <div>
       <div style="font-size: 30vh">404</div>
 
-      <div class="text-h2" style="opacity: 0.4">Oops. Nothing here...</div>
+      <div class="text-h2" style="opacity: 0.4">
+        Ой что-то не то, не такой страницы
+      </div>
 
       <q-btn
         class="q-mt-xl"
@@ -13,7 +15,7 @@
         text-color="blue"
         unelevated
         to="/"
-        label="Go Home"
+        label="На первый экран"
         no-caps
       />
     </div>
